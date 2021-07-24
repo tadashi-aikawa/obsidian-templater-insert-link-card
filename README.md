2229c1416ab77bbeff73ed66113c9c8562ac884c
tadashi-aikawa <syou.maman@gmail.com>
2021-07-24 17:04:40 +0900

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# obsidian-templater-insert-link-card
-Snippets that enable inserting a link as a card using Templater Obsidian plugin
+# Obsidian Templater Insert link card
+
+This repository is snippets that enable inserting a link as a card using [Templater], the [Obsidian] plugin.
+
+## Demo
+
+![](demo.gif)
+
+## Requirements
+
+You need to install [Obsidian] and [Templater].
+
+## Get started
+
+1⃣ Create a **Scripts directory** for using [Script User Functions] on [Templater]. For example, `_Privates/Templater/Scripts`
+
+![](1.png)
+
+2⃣ Copy or link `main.js` under the **Scripts directory**
+
+![](2.png)
+
+3⃣ Set **Scripts directory** to `Templater > Script files folder location`
+
+![](3.png)
+
+4⃣ Copy definitions of `main.css` to your CSS files (`CSS snippets` / `publish.css`)
+
+5⃣ Create a template as follows
+
+`Insert Link Card.md`
+
+```js
+<%*
+const DESC_MAX_LEN = 100
+
+const url = await tp.system.prompt("Enter URL", DESC_MAX_LEN)
+if (url) {
+  tR = await tp.user.create_card(url)
+}
+%>
+```
+
+Now, you can create a card link as HTML from the URL.
+
+[templater]: https://github.com/SilentVoid13/Templater
+[obsidian]: https://obsidian.md/
+[script user functions]: https://silentvoid13.github.io/Templater/docs/user-functions/script-user-functions
---
+++ main.js
@@ -0,0 +1,93 @@
+function trimMax(str, num) {
+  return str.length > num ? `${str.substring(0, num)}...` : str;
+}
+
+async function fetchAsDom(url) {
+  const res = await request({ url });
+  return new DOMParser().parseFromString(res, "text/html");
+}
+
+function getMetaByProperty(dom, property) {
+  return dom.querySelector(`meta[property='${property}']`)?.attributes?.content
+    ?.value;
+}
+
+function getMetaByName(dom, name) {
+  return dom.querySelector(`meta[name='${name}']`)?.attributes?.content?.value;
+}
+
+function getSrcById(dom, id) {
+  return dom.querySelector("#" + id)?.attributes?.src?.value;
+}
+
+function getFaviconUrl(dom, url) {
+  const iconHref =
+    dom.querySelector("link[rel='icon']")?.attributes?.href?.value ??
+    dom.querySelector("link[rel='shortcut icon']")?.attributes?.href?.value;
+  if (!iconHref) {
+    return new URL(url).origin + "/favicon.ico";
+  }
+
+  if (iconHref.includes("http")) {
+    return iconHref;
+  }
+  if (iconHref.startsWith("//")) {
+    return new URL(url).protocol + iconHref;
+  }
+
+  const baseUrl = dom.querySelector("base")?.attributes?.href?.value;
+  if (baseUrl) {
+    return baseUrl + iconHref;
+  }
+
+  return new URL(url).origin + iconHref;
+}
+
+function isSecure(url) {
+  return url && !url.startsWith("http://");
+}
+
+async function createCard(url, descMaxLen) {
+  const html = await fetchAsDom(url);
+
+  const siteName = getMetaByProperty(html, "og:site_name") ?? new URL(url).host;
+  const title =
+    getMetaByProperty(html, "og:title") ??
+    getMetaByProperty(html, "title") ??
+    html.querySelector("title")?.text ??
+    url;
+  const description =
+    getMetaByProperty(html, "og:description") ??
+    getMetaByProperty(html, "description") ??
+    getMetaByName(html, "description") ??
+    "";
+  const faviconUrl = getFaviconUrl(html, url);
+  const imageUrl =
+    getMetaByProperty(html, "og:image") ??
+    getSrcById(html, "ebooksImgBlkFront");
+
+  const imageDom = isSecure(imageUrl)
+    ? `<img src="${imageUrl}" class="link-card-image"/>`
+    : "";
+
+  return `<div class="link-card">
+	<div class="link-card-header">
+		<img src="${faviconUrl}" class="link-card-site-icon"/>
+		<span class="link-card-site-name">${siteName}</span>
+	</div>
+	<div class="link-card-body">
+		<div class="link-card-content">
+			<div>
+				<p class="link-card-title">${title}</p>
+			</div>
+			<div class="link-card-description">
+				${trimMax(description, descMaxLen)}
+			</div>
+		</div>
+		${imageDom}
+	</div>
+	<a href="${url}"></a>
+</div>`;
+}
+
+module.exports = createCard;
